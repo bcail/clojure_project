171b93d57cf81a9e1526b17a5d282d910688f911
Ben Cail <benjamin_cail@brown.edu>
2017-11-10 14:36:50 -0500

    add a simple function that gets a path from a pid, with a couple tests

--- src/project/core.clj
+++ src/project/core.clj
@@ -1,7 +1,26 @@
 (ns project.core
   (:gen-class))
 
+(defn path-from-pid
+  [pid]
+  (apply str ["/tmp/" (clojure.string/replace pid ":" "_")]))
+
+(defn test-path-from-pid
+  []
+  (print "  test-path-from-pid... ")
+  (if (= (path-from-pid "test:1234") "/tmp/test_1234")
+    (print "passed ")
+    (print "failed "))
+  (if (= (path-from-pid "test:4321") "/tmp/test_4321")
+    (println "passed")
+    (println "failed")))
+
+(defn run-tests
+  []
+  (println "running tests")
+  (test-path-from-pid))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Run tests"
   [& args]
-  (println "Hello, World!"))
+  (run-tests))
