c2e44b00de0765fb23878a9c9c09804b3fadf693
Ben Cail <benjamin_cail@brown.edu>
2017-11-10 16:50:57 -0500

    add sqlite deps, add code to initialize a test db

--- src/project/core.clj
+++ src/project/core.clj
@@ -1,5 +1,13 @@
 (ns project.core
-  (:gen-class))
+  (:gen-class)
+  (require [clojure.java.jdbc :as jdbc]))
+
+
+(def db-spec
+  {:classname "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname "test.db"})
+
 
 (defn path-from-pid
   [pid]
@@ -15,8 +23,24 @@
     (println "passed")
     (println "failed")))
 
+(def table-ddl
+  (jdbc/create-table-ddl "objects"
+                         [[:pid "char(20)"]
+                          [:path "char(50)"]]))
+
+(defn init-test-db
+  [db-name]
+  (try
+    (jdbc/db-do-commands db-spec
+                       [table-ddl])
+    (jdbc/insert-multi! db-spec "objects" [{:pid "test:1234" :path "/tmp/test_1234"} {:pid "test:1234" :path "/tmp/test_1234"}])
+    (catch java.lang.Exception ex))
+  )
+
 (defn run-tests
   []
+  (println "initializing DB")
+  (init-test-db "test.db")
   (println "running tests")
   (test-path-from-pid))
 
