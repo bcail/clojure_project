3db995718a9e1e95cda1eaeaf20475687184708b
Ben Cail <benjamin_cail@brown.edu>
2019-03-01 13:11:12 -0500

    use 'use-fixtures' to create test.db before each test

--- src/project/core.clj
+++ src/project/core.clj
@@ -1,40 +1,12 @@
 (ns project.core
   (:gen-class)
-  (require [clojure.java.jdbc :as jdbc]))
-
-
-(def db-spec
-  {:classname "org.sqlite.JDBC"
-   :subprotocol "sqlite"
-   :subname "test.db"})
-
+  )
 
 (defn path-from-pid
   [pid]
   (apply str ["/tmp/" (clojure.string/replace pid ":" "_")]))
 
-(def table-ddl
-  (jdbc/create-table-ddl "objects"
-                         [[:pid "char(20)"]
-                          [:path "char(50)"]]))
-
-(defn init-test-db
-  [db-name]
-  (try
-    (jdbc/db-do-commands db-spec
-                       [table-ddl])
-    (jdbc/insert-multi! db-spec "objects" [{:pid "test:1234" :path "/tmp/test_1234"} {:pid "test:1234" :path "/tmp/test_1234"}])
-    (catch java.lang.Exception ex))
-  )
-
-(defn my-run-tests
-  []
-  (println "initializing DB")
-  (init-test-db "test.db")
-  (println "running tests")
-  (my-test-path-from-pid))
-
 (defn -main
-  "Run tests"
+  "Hello World"
   [& args]
-  (my-run-tests))
+  (println "Hello World"))
--- test/project/core_test.clj
+++ test/project/core_test.clj
@@ -1,8 +1,34 @@
 (ns project.core-test
   (:require [clojure.test :refer :all]
-            [project.core :refer :all]))
+            [project.core :refer :all]
+            [clojure.java.jdbc :as jdbc]))
 
 (deftest test-path-from-pid
   (testing "basic path-from-pid functionality"
     (is (= (path-from-pid "test:1234") "/tmp/test_1234"))
     (is (= (path-from-pid "test:4321") "/tmp/test_4321"))))
+
+(def db-spec
+  {:classname "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname "test.db"})
+
+(def table-ddl
+  (jdbc/create-table-ddl "objects"
+                         [[:pid "char(20)"]
+                          [:path "char(50)"]]))
+
+(defn init-test-db
+  [db-name]
+  (try
+    (jdbc/db-do-commands db-spec
+                       [table-ddl])
+    (jdbc/insert-multi! db-spec "objects" [{:pid "test:1234" :path "/tmp/test_1234"} {:pid "test:1234" :path "/tmp/test_1234"}])
+    (catch java.lang.Exception ex))
+  )
+
+(defn db-test-fixture [f]
+  (init-test-db "test.db")
+  (f))
+
+(use-fixtures :each db-test-fixture)
